8a68ce328419a80f3b8cf7b12b94876c79933869
fross <fross.job@gmail.com>
2019-02-18 20:33:29 -0500

    Stops the intake roller before loading a balloon into the box

--- src/main/java/frc/robot/commands/elevator/LoadBalloonIntoBox.java
+++ src/main/java/frc/robot/commands/elevator/LoadBalloonIntoBox.java
@@ -1,11 +1,12 @@
 package frc.robot.commands.elevator;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.commands.balloonbox.StopIntakeRoller;
 
 public class LoadBalloonIntoBox extends CommandGroup {
 
     public LoadBalloonIntoBox() {
-        // Stop roller.
+        addSequential(new StopIntakeRoller());
         addSequential(new MoveElevatorToTopOfBox());
         addSequential(new MoveElevatorToGround());
 
